caa32e5fa5cd7ae8b6078a8aaa339ebeee7c5a54
dkagotho <dianakago2@gmail.com>
2023-01-19 15:49:57 -0500

    imported sidebar and cleared App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Malaika treats!!</h1>
+    <div className="mx-auto mt-8">
+      <SideBar />
+      <h1 className="text-4xl text-center">Malaika treats!!</h1>
     </div>
   );
 }
