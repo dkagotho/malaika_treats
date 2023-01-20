7401300f52bef0f04b530c09b416f46e5514316d
dkagotho <dianakago2@gmail.com>
2023-01-20 16:16:32 -0500

    created header component and added a link

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Header = () => {
+  let Links = [
+    {
+      name: "Curbside Pickup & Local Delivery Only - malaikatreats@gmail.com",
+      link: "/",
+    },
+  ];
+  return (
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="mb-1">
+        <ul className="text-[#778899] text-xs pt-1">
+          {Links.map((link) => (
+            <li
+              key={link.name}
+              className="mt-1 hover:text-gray-700 duration-500"
+            >
+              <a className="ml-2" href={link.link}>
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
