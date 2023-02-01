1a6f4afac310060ca917f5fef7d8244346de5c80
dkagotho <dianakago2@gmail.com>
2023-02-01 18:43:05 -0500

    did some clean up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from "react";
 import "./App.css";
 import SideBar from "./components/SideBar";
 import Header from "./components/Header";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Cakes from "./components/Cakes";
 
 function App() {
   return (
     <div>
       <SideBar />
       <Header />
+
+      {/* <BrowserRouter>
+        <Routes>
+          <Route exact path="/" component={</>} />
+          <Route path="" component={< />} />
+          <Route path="" component={< />} />
+
+        </Routes>
+      </BrowserRouter> */}
     </div>
   );
 }
